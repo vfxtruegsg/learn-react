d906d9d167c3fa1b1659f87b82239cc934154dc1
Andrij P. <andrejpopkov565@gmail.com>
2024-11-06 22:29:17 +0300

    first react lesson

---
+++ react-project/src/components/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import Product from "./Product";
+
+export default function App() {
+  return (
+    <div>
+            <h1>Products</h1>
+            <Product />
+            <Product />
+            <Product />   {" "}
+    </div>
+  );
+}
